9950e2529ca2d59b4cb052eaac6c4221f9916970
jasper-nycu <jasper.mg14@nycu.edu.tw>
2026-05-16 21:31:56 +0800

    refactor(profile): remove unused useEffect import to resolve linter warning

--- frontend/src/views/MyProfileView.tsx
+++ frontend/src/views/MyProfileView.tsx
@@ -1,5 +1,5 @@
 // src/views/MyProfileView.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { type UserProfile } from '../components/layout/Header';
 
 interface MyProfileViewProps {
