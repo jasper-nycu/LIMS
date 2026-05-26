0470b0c65bcc60e1dd1624426ecc6375d25946db
jasper-nycu <jasper.mg14@nycu.edu.tw>
2026-05-26 20:33:37 +0800

    fix: delete an unused library

--- backend/src/main/java/com/tsmc/lims/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/tsmc/lims/backend/auth/service/AuthService.java
@@ -12,7 +12,6 @@ import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
